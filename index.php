4c9bd831cd596247f2b6eccc3c94a2a531c4e604
ForestForgeron <77063357+ForestForgeron@users.noreply.github.com>
2024-06-08 13:41:53 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-0110vsvg dfsdfgdsfsd 0000
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Access-Control-Allow-Origin: *');
+
+    $data = json_decode(file_get_contents('php://input'), true);
+    // Обработка данных
+    $response = array('status' => 'success', 'message' => 'Data received');
+    echo json_encode($response);
+}
+
